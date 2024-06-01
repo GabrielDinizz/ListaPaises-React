b5588565e73b98c83fcc1c516c55d9d6c3f647ef
GabrielDinizz <gabrieldinizcarreiro@gmail.com>
2024-06-01 18:23:53 -0300

    style: home page 2.0

--- DesafioPaises/src/components/homePage/homePage.jsx
+++ DesafioPaises/src/components/homePage/homePage.jsx
@@ -8,12 +8,19 @@ function homePage() {
                 <div className='textHomePage'>
                     <h1>LISTA PAISES</h1>
                     <p>Nesta página, você encontrará uma lista completa de 250 países em ordem alfabética,
-                    com informações sobre a população e a capital de cada um. Além disso, você tem a opção
-                    de favoritar seus países preferidos e visualizar a soma total das populações desses países favoritos.</p>
+                        com informações sobre a população e a capital de cada um. Além disso, você tem a opção
+                        de favoritar seus países preferidos e visualizar a soma total das populações desses países favoritos.</p>
                     <button>Vizualizar</button>
                 </div>
                 <div className='divImgHomePage'>
                     <img src={img} alt="" />
+                </div>
+            </section>
+            <section className='containerDesafio'>
+                <div>
+                    <h2>Desafio para você!</h2>
+                    <p>Consegue atingir uma população total de mais de 1 bilhão de pessoas selecionando
+                    apenas três países? Boa sorte e divirta-se explorando!</p>
                 </div>
             </section>
         </>
